42e1d631018a0f3ade6a71ce780547f847a64d68
Vis Vasanth <viscodesdpt@gmail.com>
2017-01-27 13:27:32 -0600

    semi responsive navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,18 +14,31 @@ render: function(){
 <div className="navContainer">
 
 
-<nav className="navbar navbar-inverse">
-  <div className="container-fluid">
-    <div className="navbar-header">
-      <a className="navbar-brand" href="#" id="navTitle">COLLABORATE</a>
-    </div>
+    <nav id="mainNav" className="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div className="container">
 
-    <ul className="nav navbar-nav navbar-right">
-      <li> <a href="login.html" id="login-register">Sign Up/ LogIn</a></li>
-      
-    </ul>
-  </div>
-</nav>
+            <div className="navbar-header page-scroll">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span className="sr-only">Toggle navigation</span> Menu <i className="fa fa-bars"></i>
+                </button>
+                <a className="navbar-brand" href="#page-top" id="navTitle">COLLABORATE</a>
+            </div>
+
+
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav navbar-right">
+                    <li className="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li className="page-scroll">
+                        <a href="login.html" id="login-register">Sign Up/ LogIn</a>
+                    </li>
+                </ul>
+            </div>
+        
+        </div>
+        
+    </nav>
    
    </div> 
       	)
