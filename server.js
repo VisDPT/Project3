06f318dc7907c5a51456d5debc8e72649efee6e7
Vis Vasanth <viscodesdpt@gmail.com>
2017-01-28 13:32:56 -0600

    heroku plz work

--- server.js
+++ server.js
@@ -223,6 +223,38 @@ app.put('/user/:documentid/', function(req, res) {
 
 
 
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err,
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+    });
+});
+
+
 //==================== SERVER ========================
 app.listen(PORT, function() {
     console.log('24k Magic happens on port ' + PORT);
